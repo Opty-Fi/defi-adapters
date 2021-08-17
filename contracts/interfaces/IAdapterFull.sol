3e653ef47d437d165f721f2caddf3ebe234db75d
dhruvinparikh <imsilent93@gmail.com>
2021-08-17 12:51:38 -0400

    feat(defi adapter for opty.fi): defi Adapters to be implemented for inegrating with opty.fi's vault

---
+++ contracts/interfaces/IAdapterFull.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+import { IAdapter } from "./IAdapter.sol";
+import { IAdapterBorrow } from "./IAdapterBorrow.sol";
+import { IAdapterHarvestReward } from "./IAdapterHarvestReward.sol";
+import { IAdapterStaking } from "./IAdapterStaking.sol";
+import { IAdapterStakingCurve } from "./IAdapterStakingCurve.sol";
+import { IAdapterInvestLimit } from "./IAdapterInvestLimit.sol";
+
+/**
+ * @title Interface containing all functions from different DeFi adapter interfaces
+ * @author Opty.fi
+ * @notice Interface of the DeFi protocol adapter to be used where all DeFi adapter features are required
+ * @dev Abstraction layer to different tokenization contracts like StrategyManager etc.
+ * It can also be used as an interface layer for any new DeFi protocol. It contains all the
+ * functions being used in all the DeFi adapters from different interfaces
+ */
+
+/* solhint-disable no-empty-blocks */
+interface IAdapterFull is
+    IAdapter,
+    IAdapterBorrow,
+    IAdapterHarvestReward,
+    IAdapterStaking,
+    IAdapterStakingCurve,
+    IAdapterInvestLimit
+{
+
+}
+/* solhint-disable no-empty-blocks */
