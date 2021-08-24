37eaca9b525a43c511b6fa44a2ec520557382624
dhruvinparikh <imsilent93@gmail.com>
2021-08-24 16:40:36 -0400

    refactor: actual and expected value unit test

--- test/adapters/HarvestFinanceAdapter.behavior.ts
+++ test/adapters/HarvestFinanceAdapter.behavior.ts
@@ -6,22 +6,105 @@ import { PoolItem } from "../types";
 chai.use(solidity);
 
 export function shouldBehaveLikeHarvestFinanceAdapter(token: string, pool: PoolItem): void {
-  it(`should deposit and withdraw ${token} in ${token} pool of Harvest Finance`, async function () {
+  it(`should deposit ${token}, stake f${token}, claim FARM, harvest FARM, unstake f${token}, and withdraw f${token} in ${token} pool of Harvest Finance`, async function () {
+    // harvest finance's deposit vault instance
+    const harvestDepositInstance = await hre.ethers.getContractAt("IHarvestDeposit", pool.pool);
+
+    // harvest finance's staking vault instance
+    const harvestStakingInstance = await hre.ethers.getContractAt("IHarvestFarm", pool.stakingPool as string);
+
+    // harvest finance reward token's instance
+    const farmRewardInstance = await hre.ethers.getContractAt("IERC20", (pool.rewardTokens as string[])[0]);
+
+    // underlying token instance
+    const underlyingTokenInstance = await hre.ethers.getContractAt("IERC20", pool.tokens[0]);
+
+    // 1. deposit all underlying tokens
     await this.testDeFiAdapter.testGetDepositAllCodes(pool.tokens[0], pool.pool, this.harvestFinanceAdapter.address);
-    expect(
-      await this.harvestFinanceAdapter.getLiquidityPoolTokenBalance(
-        this.testDeFiAdapter.address,
-        this.testDeFiAdapter.address,
-        pool.pool,
-      ),
-    ).to.be.gt(0);
+    // 1.1 assert whether lptoken balance is as expected or not after deposit
+    const actualLPTokenBalanceAfterDeposit = await this.harvestFinanceAdapter.getLiquidityPoolTokenBalance(
+      this.testDeFiAdapter.address,
+      this.testDeFiAdapter.address, // placeholder of type address
+      pool.pool,
+    );
+    const expectedLPTokenBalanceAfterDeposit = await harvestDepositInstance.balanceOf(this.testDeFiAdapter.address);
+    expect(actualLPTokenBalanceAfterDeposit).to.be.eq(expectedLPTokenBalanceAfterDeposit);
+    // 1.2 assert whether underlying token balance is as expected or not
+    const actualUnderlyingTokenBalanceAfterDeposit = await this.testDeFiAdapter.getERC20TokenBalance(
+      (
+        await this.harvestFinanceAdapter.getUnderlyingTokens(pool.pool, pool.pool)
+      )[0],
+      this.testDeFiAdapter.address,
+    );
+    const expectedUnderlyingTokenBalanceAfterDeposit = await underlyingTokenInstance.balanceOf(
+      this.testDeFiAdapter.address,
+    );
+    expect(actualUnderlyingTokenBalanceAfterDeposit).to.be.eq(expectedUnderlyingTokenBalanceAfterDeposit);
+
+    // 2. stake all lpTokens
+    await this.testDeFiAdapter.testGetStakeAllCodes(pool.pool, pool.tokens[0], this.harvestFinanceAdapter.address);
+    // 2.1 assert whether the staked lpToken balance is as expected or not
+    const actualStakedLPTokenBalanceAfterStake = await this.harvestFinanceAdapter.getLiquidityPoolTokenBalanceStake(
+      this.testDeFiAdapter.address,
+      pool.pool,
+    );
+    const expectedStakedLPTokenBalanceAfterStake = await harvestStakingInstance.balanceOf(this.testDeFiAdapter.address);
+    expect(actualStakedLPTokenBalanceAfterStake).to.be.eq(expectedStakedLPTokenBalanceAfterStake);
+
+    // 3. claim the reward token
+    await this.testDeFiAdapter.testClaimRewardTokenCode(pool.pool, this.harvestFinanceAdapter.address);
+    // 3.1 assert whether the reward token's balance is as expected or not
+    const actualRewardTokenBalanceAfterClaim = await this.testDeFiAdapter.getERC20TokenBalance(
+      await this.harvestFinanceAdapter.getRewardToken(pool.pool),
+      this.testDeFiAdapter.address,
+    );
+    const expectedRewardTokenBalanceAfterClaim = await farmRewardInstance.balanceOf(this.testDeFiAdapter.address);
+    expect(actualRewardTokenBalanceAfterClaim).to.be.eq(expectedRewardTokenBalanceAfterClaim);
+
+    // 4. Swap the reward token into underlying token
+    await this.testDeFiAdapter.testGetHarvestAllCodes(pool.pool, pool.tokens[0], this.harvestFinanceAdapter.address);
+    expect(await this.testDeFiAdapter.getERC20TokenBalance(pool.tokens[0], this.testDeFiAdapter.address)).to.be.gt(0);
+
+    // 5. Unstake all staked lpTokens
+    await this.testDeFiAdapter.testGetUnstakeAllCodes(pool.pool, this.harvestFinanceAdapter.address);
+    // 5.1 assert whether lpToken balance is as expected or not
+    const actualLPTokenBalanceAfterUnstake = await this.harvestFinanceAdapter.getLiquidityPoolTokenBalance(
+      this.testDeFiAdapter.address,
+      this.testDeFiAdapter.address, // placeholder of type address
+      pool.pool,
+    );
+    const expectedLPTokenBalanceAfterUnstake = await harvestDepositInstance.balanceOf(this.testDeFiAdapter.address);
+    expect(actualLPTokenBalanceAfterUnstake).to.be.eq(expectedLPTokenBalanceAfterUnstake);
+    // 5.2 assert whether staked lpToken balance is as expected or not
+    const actualStakedLPTokenBalanceAfterUnstake = await this.harvestFinanceAdapter.getLiquidityPoolTokenBalanceStake(
+      this.testDeFiAdapter.address,
+      pool.pool,
+    );
+    const expectedStakedLPTokenBalanceAfterUnstake = await harvestStakingInstance.balanceOf(
+      this.testDeFiAdapter.address,
+    );
+    expect(actualStakedLPTokenBalanceAfterUnstake).to.be.eq(expectedStakedLPTokenBalanceAfterUnstake);
+
+    // 6. Withdraw all lpToken balance
     await this.testDeFiAdapter.testGetWithdrawAllCodes(pool.tokens[0], pool.pool, this.harvestFinanceAdapter.address);
-    expect(
-      await this.harvestFinanceAdapter.getLiquidityPoolTokenBalance(
-        this.testDeFiAdapter.address,
-        this.testDeFiAdapter.address,
-        pool.pool,
-      ),
-    ).to.be.eq(0);
+    // 6.1 assert whether lpToken balance is as expected or not
+    const actualLPTokenBalanceAfterWithdraw = await this.harvestFinanceAdapter.getLiquidityPoolTokenBalance(
+      this.testDeFiAdapter.address,
+      this.testDeFiAdapter.address, // placeholder of type address
+      pool.pool,
+    );
+    const expectedLPTokenBalanceAfterWithdraw = await harvestDepositInstance.balanceOf(this.testDeFiAdapter.address);
+    expect(actualLPTokenBalanceAfterWithdraw).to.be.eq(expectedLPTokenBalanceAfterWithdraw);
+    // 6.2 assert whether underlying token balance is as expected or not after withdraw
+    const actualUnderlyingTokenBalanceAfterWithdraw = await this.testDeFiAdapter.getERC20TokenBalance(
+      (
+        await this.harvestFinanceAdapter.getUnderlyingTokens(pool.pool, pool.pool)
+      )[0],
+      this.testDeFiAdapter.address,
+    );
+    const expectedUnderlyingTokenBalanceAfterWithdraw = await underlyingTokenInstance.balanceOf(
+      this.testDeFiAdapter.address,
+    );
+    expect(actualUnderlyingTokenBalanceAfterWithdraw).to.be.eq(expectedUnderlyingTokenBalanceAfterWithdraw);
   });
 }
--- test/adapters/HarvestFinanceAdapter.ts
+++ test/adapters/HarvestFinanceAdapter.ts
@@ -52,10 +52,13 @@ describe("Unit tests", function () {
     // fund TestDeFiAdapter with 10000 tokens each
     await dai.transfer(this.testDeFiAdapter.address, hre.ethers.utils.parseEther("10000"));
     await usdt.transfer(this.testDeFiAdapter.address, hre.ethers.utils.parseUnits("10000", 6));
+
+    // whitelist TestDeFiAdapter contract into HarvestFinance's Vaults
+    // by impersonating the governance's address
     const tokenNames = Object.keys(HarvestFinancePools);
     for (const tokenName of tokenNames) {
       const { pool } = (HarvestFinancePools as LiquidityPool)[tokenName];
-      const harvestVault = await hre.ethers.getContractAt("IHarvestVault", pool);
+      const harvestVault = await hre.ethers.getContractAt("IHarvestDeposit", pool);
       const governance = await harvestVault.governance();
       await hre.network.provider.request({
         method: "hardhat_impersonateAccount",
