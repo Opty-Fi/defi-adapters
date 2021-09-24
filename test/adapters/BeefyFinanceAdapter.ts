464294691163d84681ce475ba3369c75710c77e9
Richard Jamieson <jamieson.rich@gmail.com>
2021-09-24 16:19:33 +0200

    fix: tidying up

--- test/adapters/BeefyFinanceAdapter.ts
+++ test/adapters/BeefyFinanceAdapter.ts
@@ -6,7 +6,7 @@ import { TestDeFiAdapter } from "../../typechain/TestDeFiAdapter";
 import { LiquidityPool, Signers } from "../types";
 import { shouldBehaveLikeBeefyFinanceAdapter } from "./BeefyFinanceAdapter.behavior";
 // import { shouldStakeLikeBeefyFinanceAdapter } from "./BeefyFinanceAdapter.behavior";
-import { default as BeefyFinanceLiquidityPools } from "../beefy_quickswap_liquidity_pools.json";
+import { default as BeefyFinanceLiquidityPools } from "../beefy_dfyn_liquidity_pools.json";
 // import { default as BeefyStakingPools } from "../beefy.staking-pools.json";
 import { IUniswapV2Router02 } from "../../typechain";
 import { getOverrideOptions } from "../utils";
@@ -38,88 +38,109 @@ describe("Unit tests", function () {
 
     for (const pool of Object.values(BeefyFinanceLiquidityPools)) {
       const LP_TOKEN_CONTRACT = await hre.ethers.getContractAt("IUniswapV2Pair", pool.wantToken);
-      let hostRouterAddress, swapRouterAddress;
+      let hostRouterAddress, swapRouterAddress, wmatic_address;
       switch (pool.platform) {
         case "QuickSwap":
           swapRouterAddress = "0xa5e0829caced8ffdd4de3c43696c57f7d7a678ff";
           hostRouterAddress = "0xa5e0829caced8ffdd4de3c43696c57f7d7a678ff";
+          wmatic_address = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
           break;
         case "JetSwap":
           swapRouterAddress = "0x5C6EC38fb0e2609672BDf628B1fD605A523E5923";
           hostRouterAddress = "0x5C6EC38fb0e2609672BDf628B1fD605A523E5923";
+          wmatic_address = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
           break;
         case "SushiSwap":
           swapRouterAddress = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
           hostRouterAddress = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
+          wmatic_address = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
           break;
         case "ApeSwap":
           swapRouterAddress = "0xC0788A3aD43d79aa53B09c2EaCc313A787d1d607";
           hostRouterAddress = "0xC0788A3aD43d79aa53B09c2EaCc313A787d1d607";
+          wmatic_address = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
           break;
         case "Cometh":
           swapRouterAddress = "0x93bcDc45f7e62f89a8e901DC4A0E2c6C427D9F25";
           hostRouterAddress = "0x93bcDc45f7e62f89a8e901DC4A0E2c6C427D9F25";
+          wmatic_address = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
           break;
         case "DFyn":
           swapRouterAddress = "0xa5e0829caced8ffdd4de3c43696c57f7d7a678ff"; //trying quickswap here...
           hostRouterAddress = "0xA102072A4C07F06EC3B4900FDC4C7B80b6c57429";
+          wmatic_address = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"; //Dfyn use a different WMATIC to most others: 0x4c28f48448720e9000907bc2611f73022fdce1fa
           break;
         case "WaultFinance":
           swapRouterAddress = "0x3a1D87f206D12415f5b0A33E786967680AAb4f6d";
           hostRouterAddress = "0x3a1D87f206D12415f5b0A33E786967680AAb4f6d";
+          wmatic_address = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
           break;
         case "Polyzap":
           swapRouterAddress = "0x4aAEC1FA8247F85Dc3Df20F4e03FEAFdCB087Ae9";
           hostRouterAddress = "0x4aAEC1FA8247F85Dc3Df20F4e03FEAFdCB087Ae9";
+          wmatic_address = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
           break;
         case "FireBird":
           swapRouterAddress = "0xa5e0829caced8ffdd4de3c43696c57f7d7a678ff"; //still failing with QS as swapRouter - failing at addLiq?
           hostRouterAddress = "0xF6fa9Ea1f64f1BBfA8d71f7f43fAF6D45520bfac";
+          wmatic_address = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
           break;
         case "Curve":
           swapRouterAddress = "0xa5e0829caced8ffdd4de3c43696c57f7d7a678ff"; //Quickswap for the swaps - can't swap from wmatic in curve
           hostRouterAddress = "0xF6fa9Ea1f64f1BBfA8d71f7f43fAF6D45520bfac";
+          wmatic_address = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
           break;
         default:
           swapRouterAddress = "0xa5e0829caced8ffdd4de3c43696c57f7d7a678ff";
           hostRouterAddress = "0xa5e0829caced8ffdd4de3c43696c57f7d7a678ff";
+          wmatic_address = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
           break;
       }
       // get the UniswapV2Router contract instance that hosts this liquidity pool
       this.swapRouter = <IUniswapV2Router02>await hre.ethers.getContractAt("IUniswapV2Router02", swapRouterAddress); //changed this to polygon apeswap router
-
+      console.log("swaprouter alive");
       //get the addresses of the two respective tokens
       const token0_address = await LP_TOKEN_CONTRACT.token0();
       const token1_address = await LP_TOKEN_CONTRACT.token1();
-      const wmatic_address = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
+      console.log(token0_address);
 
       // swap 100 MATIC for token0 and token1 respectively
       if (token0_address == hre.ethers.utils.getAddress(wmatic_address)) {
         const wmatic_token = await hre.ethers.getContractAt("IWETH", token0_address);
-        await wmatic_token.deposit({ value: hre.ethers.utils.parseEther("100") });
+        await wmatic_token.deposit({ value: hre.ethers.utils.parseEther("50") });
       } else {
-        await this.swapRouter.swapExactETHForTokens(
-          0,
-          [wmatic_address, token0_address],
-          this.signers.admin.address,
-          Date.now() + 900,
-          { value: hre.ethers.utils.parseEther("50") },
-        );
+        try {
+          await this.swapRouter.swapExactETHForTokens(
+            0,
+            [wmatic_address, token0_address],
+            this.signers.admin.address,
+            Date.now() + 900,
+            { value: hre.ethers.utils.parseEther("50") },
+          );
+          console.log("swap1 done");
+        } catch (err) {
+          console.log("First swap failed");
+          console.log(err);
+        }
       }
-
       if (token1_address == hre.ethers.utils.getAddress(wmatic_address)) {
         const wmatic_token = await hre.ethers.getContractAt("IWETH", token1_address);
-        await wmatic_token.deposit({ value: hre.ethers.utils.parseEther("100") });
+        await wmatic_token.deposit({ value: hre.ethers.utils.parseEther("50") });
       } else {
-        await this.swapRouter.swapExactETHForTokens(
-          0,
-          [wmatic_address, token1_address],
-          this.signers.admin.address,
-          Date.now() + 900,
-          { value: hre.ethers.utils.parseEther("50") },
-        );
+        try {
+          await this.swapRouter.swapExactETHForTokens(
+            0,
+            [wmatic_address, token1_address],
+            this.signers.admin.address,
+            Date.now() + 900,
+            { value: hre.ethers.utils.parseEther("50") },
+          );
+          console.log("swap2 done");
+        } catch (err) {
+          console.log("Second swap failed");
+          console.log(err);
+        }
       }
-
       // get the UniswapV2Router contract instance that hosts this liquidity pool
       this.hostRouter = <IUniswapV2Router02>await hre.ethers.getContractAt("IUniswapV2Router02", hostRouterAddress); //changed this to polygon apeswap router
 
@@ -132,7 +153,7 @@ describe("Unit tests", function () {
       const token1 = await hre.ethers.getContractAt("ERC20", token1_address);
       var token1Balance = await token1.balanceOf(this.signers.admin.address);
       await token1.approve(this.hostRouter.address, token1Balance);
-
+      console.log("approvals done");
       //add liquidity to get LP tokens for deposit
       await this.hostRouter.addLiquidity(
         token0_address,
@@ -144,7 +165,7 @@ describe("Unit tests", function () {
         this.signers.admin.address,
         Date.now() + 900,
       );
-
+      console.log("liquidity added");
       const initialLPtokenBalance = await LP_TOKEN_CONTRACT.balanceOf(this.signers.admin.address);
 
       // fund TestDeFiAdapter with initialLPtokenBalance
